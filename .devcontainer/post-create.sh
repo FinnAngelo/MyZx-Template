4011351278efa318f983a280c5d4222b74de23dd
FinnAngelo <jon@finnangelo.com>
2022-06-23 15:51:08 +1200

    Setup sjasmplus for the container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo Howdy
+echo "Setup sjasmplus for the container"
+
+unzip ~/../../workspaces/*/tools/sjasmplus.1.18.3.zip -d ~/../../workspaces/*/tools
+cd ~/../../workspaces/*/tools/sjasmplus-1.18.3
+make clean
+make
+sudo make install
+
+rm -rf ~/../tools/sjasmplus-1.18.3
